fa3a1960a56d95b731d9261dc008edb22b4068df
Maicon-Moreira <maiconmoreirapira@gmail.com>
2020-10-19 15:52:20 -0300

    basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+main() {
+  MyApp app = MyApp();
+
+  runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('EasyList'),
+      ),
+    ));
+  }
+}
